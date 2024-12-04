2e3808d6af23d89f69adb7d01ce441b68927bc1b
lee <dragon02229@naver.com>
2024-12-05 02:26:53 +0900

    모든 리스트, 최근 읽은 리스트 fragment 생성 및 tab select시, fragment띄우는 로직 구현

--- app/src/main/java/com/yongjincompany/android_assignment/MainActivity.kt
+++ app/src/main/java/com/yongjincompany/android_assignment/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val tabLayout: TabLayout = findViewById(R.id.tl_list_tab)
+
+        //init fragment
+        replaceFragment(RecentReadFragment())
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> {
+                        replaceFragment(RecentReadFragment())
+                    }
+
+                    1 -> {
+                        replaceFragment(AllListFragment())
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
