180b63700fbd7dffbd0fa1a965d98e13a22c3328
lee <dragon02229@naver.com>
2024-12-24 18:48:26 +0900

    RecentReadCardListFragment에 viewBinding적용 및 api호출 로직 구현

--- app/src/main/java/com/yongjincompany/android_assignment/feature/home/RecentReadCardListFragment.kt
+++ app/src/main/java/com/yongjincompany/android_assignment/feature/home/RecentReadCardListFragment.kt
@@ -1,53 +1,76 @@
 package com.yongjincompany.android_assignment.feature.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.Toast
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.recyclerview.widget.RecyclerView
-import com.yongjincompany.android_assignment.data.RecentReadCardListManager
 import com.yongjincompany.android_assignment.R
 import com.yongjincompany.android_assignment.core.util.SpacingItemDecoration
+import com.yongjincompany.android_assignment.core.util.repeatOnLifecycleState
+import com.yongjincompany.android_assignment.core.util.toast
+import com.yongjincompany.android_assignment.data.RetrofitBuilder
+import com.yongjincompany.android_assignment.databinding.FragmentRecentReadCardListBinding
 import com.yongjincompany.android_assignment.feature.home.adapter.RecentReadCardListAdapter
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class RecentReadCardListFragment : Fragment(R.layout.fragment_recent_read_card_list) {
-    private var recentReadCardListAdapter: RecentReadCardListAdapter? = null
+    private var _binding: FragmentRecentReadCardListBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var recentReadCardListAdapter: RecentReadCardListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecentReadCardListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val recentReadCardListRecyclerView: RecyclerView =
-            view.findViewById(R.id.rv_recent_read_card_list)
-
         recentReadCardListAdapter = RecentReadCardListAdapter()
-        recentReadCardListRecyclerView.adapter = recentReadCardListAdapter
-        recentReadCardListRecyclerView.addItemDecoration(SpacingItemDecoration(30))
+        binding.rvRecentReadCardList.adapter = recentReadCardListAdapter
+        binding.rvRecentReadCardList.addItemDecoration(SpacingItemDecoration(30))
 
-        recentReadCardListAdapter?.submitList(RecentReadCardListManager.recentReadCardList)
+        repeatOnLifecycleState {
+            runCatching {
+                RetrofitBuilder.cardApi.fetchRecentReadCardList()
+            }.onSuccess {
+                recentReadCardListAdapter.submitList(it)
+            }.onFailure {
+                requireContext().toast(getString(R.string.cant_fetch_card_list))
+            }
+        }
 
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                while (true) {
-                    delay(10000)
-                    RecentReadCardListManager.removeRecentFirstReadCard(
-                        {
-                            recentReadCardListAdapter?.submitList(RecentReadCardListManager.recentReadCardList)
-                            Toast
-                                .makeText(
-                                    requireContext(),
-                                    getString(R.string.remove_card_notice),
-                                    Toast.LENGTH_SHORT
-                                )
-                                .show()
-                        }
-                    )
+        repeatOnLifecycleState(Lifecycle.State.RESUMED) {
+            while (true) {
+                delay(10000)
+                if (recentReadCardListAdapter.currentList.isNotEmpty()) {
+                    runCatching {
+                        RetrofitBuilder.cardApi.changeCardReadStatus(
+                            recentReadCardListAdapter.currentList[0].id,
+                            false
+                        )
+                    }.onSuccess {
+                        recentReadCardListAdapter.removeFirstItem()
+                        requireContext().toast(getString(R.string.remove_card_notice))
+                    }.onFailure {
+                        requireContext().toast(getString(R.string.cant_save_card_read))
+                    }
+                } else {
+                    break
                 }
             }
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
