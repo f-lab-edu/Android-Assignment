dc6b9c6bce1d0dd07d044a1d9536e0e191081338
lee <dragon02229@naver.com>
2025-01-03 23:40:11 +0900

    api 요청 시 최근 읽은 카드 목록 ViewModel을 사용하도록 수정

--- app/src/main/java/com/yongjincompany/android_assignment/feature/home/RecentReadCardListFragment.kt
+++ app/src/main/java/com/yongjincompany/android_assignment/feature/home/RecentReadCardListFragment.kt
@@ -6,19 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
 import com.yongjincompany.android_assignment.R
 import com.yongjincompany.android_assignment.core.util.SpacingItemDecoration
 import com.yongjincompany.android_assignment.core.util.repeatOnLifecycleState
 import com.yongjincompany.android_assignment.core.util.toast
-import com.yongjincompany.android_assignment.data.RetrofitBuilder
+import com.yongjincompany.android_assignment.data.RepositoryBuilder
 import com.yongjincompany.android_assignment.databinding.FragmentRecentReadCardListBinding
 import com.yongjincompany.android_assignment.feature.home.adapter.RecentReadCardListAdapter
+import com.yongjincompany.android_assignment.feature.home.viewmodel.ChangeReadStatusSuccess
+import com.yongjincompany.android_assignment.feature.home.viewmodel.FetchRecentReadCardListSuccess
+import com.yongjincompany.android_assignment.feature.home.viewmodel.RecentReadCardListFailed
+import com.yongjincompany.android_assignment.feature.home.viewmodel.RecentReadCardListViewModel
+import com.yongjincompany.android_assignment.feature.home.viewmodel.RecentReadCardListViewModelFactory
 import kotlinx.coroutines.delay
 
-class RecentReadCardListFragment : Fragment(R.layout.fragment_recent_read_card_list) {
+class RecentReadCardListFragment : Fragment() {
     private var _binding: FragmentRecentReadCardListBinding? = null
     private val binding get() = _binding!!
     private lateinit var recentReadCardListAdapter: RecentReadCardListAdapter
+    private lateinit var vm: RecentReadCardListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,37 +40,49 @@ class RecentReadCardListFragment : Fragment(R.layout.fragment_recent_read_card_l
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        init()
+        observe()
+    }
+
+    private fun init() {
+        val viewModelFactory = RecentReadCardListViewModelFactory(RepositoryBuilder.cardRepository)
+        vm = ViewModelProvider(this, viewModelFactory)[RecentReadCardListViewModel::class.java]
+
         recentReadCardListAdapter = RecentReadCardListAdapter()
         binding.rvRecentReadCardList.adapter = recentReadCardListAdapter
         binding.rvRecentReadCardList.addItemDecoration(SpacingItemDecoration(30))
 
-        repeatOnLifecycleState {
-            runCatching {
-                RetrofitBuilder.cardApi.fetchRecentReadCardList()
-            }.onSuccess {
-                recentReadCardListAdapter.submitList(it)
-            }.onFailure {
-                requireContext().toast(getString(R.string.cant_fetch_card_list))
-            }
-        }
+        vm.fetchRecentReadCardList()
+    }
 
-        repeatOnLifecycleState(Lifecycle.State.RESUMED) {
-            while (true) {
-                delay(10000)
-                if (recentReadCardListAdapter.currentList.isNotEmpty()) {
-                    runCatching {
-                        RetrofitBuilder.cardApi.changeCardReadStatus(
-                            recentReadCardListAdapter.currentList[0].id,
-                            false
-                        )
-                    }.onSuccess {
+    private fun observe() {
+        repeatOnLifecycleState {
+            vm.event.collect {
+                when (it) {
+                    ChangeReadStatusSuccess -> {
                         recentReadCardListAdapter.removeFirstItem()
                         requireContext().toast(getString(R.string.remove_card_notice))
-                    }.onFailure {
-                        requireContext().toast(getString(R.string.cant_save_card_read))
                     }
-                } else {
-                    break
+
+                    is FetchRecentReadCardListSuccess -> {
+                        recentReadCardListAdapter.submitList(it.data)
+
+                        repeatOnLifecycleState(Lifecycle.State.RESUMED) {
+                            while (true) {
+                                delay(10000)
+                                if (recentReadCardListAdapter.currentList.isNotEmpty()) {
+                                    vm.changeCardReadStatus(
+                                        recentReadCardListAdapter.currentList[0].id,
+                                        false
+                                    )
+                                } else {
+                                    break
+                                }
+                            }
+                        }
+                    }
+
+                    is RecentReadCardListFailed -> requireContext().toast(getString(it.errorMessage))
                 }
             }
         }
