df1115c0edf3951094e5fbfa444c72ffc98339ff
lee <dragon02229@naver.com>
2025-01-03 23:40:26 +0900

    api 요청 시 카드상세 ViewModel을 사용하도록 수정

--- app/src/main/java/com/yongjincompany/android_assignment/feature/home/CardDetailActivity.kt
+++ app/src/main/java/com/yongjincompany/android_assignment/feature/home/CardDetailActivity.kt
@@ -2,15 +2,20 @@ package com.yongjincompany.android_assignment.feature.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.yongjincompany.android_assignment.R
 import com.yongjincompany.android_assignment.core.util.repeatOnLifecycleState
 import com.yongjincompany.android_assignment.core.util.toast
-import com.yongjincompany.android_assignment.data.RetrofitBuilder
+import com.yongjincompany.android_assignment.data.RepositoryBuilder
 import com.yongjincompany.android_assignment.databinding.ActivityCardDetailBinding
+import com.yongjincompany.android_assignment.feature.home.viewmodel.CardDetailFailed
+import com.yongjincompany.android_assignment.feature.home.viewmodel.CardDetailViewModel
+import com.yongjincompany.android_assignment.feature.home.viewmodel.CardDetailViewModelFactory
+import com.yongjincompany.android_assignment.feature.home.viewmodel.ChangeCardReadStatusSuccess
 
 class CardDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCardDetailBinding
+    private lateinit var vm : CardDetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,34 +23,45 @@ class CardDetailActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        init()
+        observe()
+    }
+
+    private fun init() {
         val receivedCardId = intent.getLongExtra("card_id", 0)
-        val receivedCardName = intent.getStringExtra("card_name")
-        val receivedCardImg = intent.getStringExtra("card_img")
-        val receivedCardGrade = intent.getStringExtra("card_grade")
-        val receivedCardDescription = intent.getStringExtra("card_description")
 
-        repeatOnLifecycleState {
-            runCatching {
-                RetrofitBuilder.cardApi.changeCardReadStatus(
-                    receivedCardId,
-                    true
-                )
-            }.onFailure {
-                baseContext.toast(getString(R.string.cant_save_card_read))
-            }
-        }
+        val viewModelFactory = CardDetailViewModelFactory(RepositoryBuilder.cardRepository)
+        vm = ViewModelProvider(this, viewModelFactory)[CardDetailViewModel::class.java]
 
-        binding.tvCardId.text = receivedCardId.toString()
-        binding.tvCardName.text = receivedCardName
-        binding.tvCardGrade.text = receivedCardGrade
-        binding.tvCardDescription.text = receivedCardDescription
-        Glide.with(this)
-            .load(receivedCardImg)
-            .into(binding.ivCardImg)
+        vm.changeCardReadStatus(
+            receivedCardId,
+            true
+        )
 
         binding.ivBack.setOnClickListener {
             finish()
         }
+    }
 
+    private fun observe() {
+        repeatOnLifecycleState {
+            vm.event.collect {
+                when(it) {
+                    is ChangeCardReadStatusSuccess -> {
+                        binding.tvCardId.text = it.data.id.toString()
+                        binding.tvCardName.text = it.data.name
+                        binding.tvCardGrade.text = it.data.grade.toString()
+                        binding.tvCardDescription.text = it.data.description
+                        Glide.with(this@CardDetailActivity)
+                            .load(it.data.imageUrl)
+                            .into(binding.ivCardImg)
+                    }
+
+                    is CardDetailFailed -> {
+                        baseContext.toast(getString(it.errorMessage))
+                    }
+                }
+            }
+        }
     }
 }
