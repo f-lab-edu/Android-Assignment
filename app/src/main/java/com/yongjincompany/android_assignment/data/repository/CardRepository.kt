1ac6330b351a0fe207d7f7adcc072c207019e074
lee <dragon02229@naver.com>
2025-01-07 19:43:34 +0900

    repository에서 모든 카드 리스트 받아오는 함수 flow로 캐싱로직 emit하도록 수정

--- app/src/main/java/com/yongjincompany/android_assignment/data/repository/CardRepository.kt
+++ app/src/main/java/com/yongjincompany/android_assignment/data/repository/CardRepository.kt
@@ -1,11 +1,31 @@
 package com.yongjincompany.android_assignment.data.repository
 
+import com.yongjincompany.android_assignment.data.local.dao.CardDao
+import com.yongjincompany.android_assignment.data.mapper.toCardEntity
+import com.yongjincompany.android_assignment.data.mapper.toCardResponse
 import com.yongjincompany.android_assignment.data.remote.api.CardApi
 import com.yongjincompany.android_assignment.data.remote.model.response.Card
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
 
-class CardRepository(private val cardApi: CardApi) {
-    suspend fun fetchAllCardList(): Result<List<Card>> =
-        kotlin.runCatching { cardApi.fetchAllCardList() }
+class CardRepository @Inject constructor(
+    private val cardApi: CardApi,
+    private val cardDao: CardDao
+) {
+    fun fetchAllCardList(): Flow<List<Card>> = flow {
+        runCatching {
+            cardApi.fetchAllCardList()
+        }.onSuccess {
+            emit(it)
+            cardDao.insertCards(it.map { card -> card.toCardEntity() })
+        }.onFailure {
+            val localData = cardDao.selectAllFromCardsTable().map { it.toCardResponse() }
+            emit(localData)
+        }
+    }.flowOn(Dispatchers.IO)
 
     suspend fun fetchRecentReadCardList(): Result<List<Card>> =
         kotlin.runCatching { cardApi.fetchRecentReadCardList() }
