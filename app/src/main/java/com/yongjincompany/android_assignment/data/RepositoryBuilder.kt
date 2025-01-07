9cf26966b0d0ca6db32abf68f70c2e10221b35b2
lee <dragon02229@naver.com>
2025-01-07 19:44:09 +0900

    기존 Builder파일 제거

--- app/src/main/java/com/yongjincompany/android_assignment/data/RepositoryBuilder.kt
+++
@@ -1,8 +0,0 @@
-package com.yongjincompany.android_assignment.data
-
-import com.yongjincompany.android_assignment.data.RetrofitBuilder.cardApi
-import com.yongjincompany.android_assignment.data.repository.CardRepository
-
-object RepositoryBuilder {
-    val cardRepository = CardRepository(cardApi)
-}
